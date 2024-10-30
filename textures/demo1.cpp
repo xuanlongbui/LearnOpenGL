7093e97193a8f752e26af1f04b0937846c2d57ce
xuanlongbui <xuanlong1372000@gmail.com>
2024-10-30 14:32:30 +0700

    transformation

--- textures/demo1.cpp
+++ textures/demo1.cpp
@@ -49,7 +49,7 @@ int main()
 
     // build and compile our shader zprogram
     // ------------------------------------
-    Shader ourShader("/home/test/share/C/opengl/textures/resources/texture.vs", "/home/test/share/C/opengl/textures/resources/texture.fs"); 
+    Shader ourShader("/home/test/share/C/opengl/resources/textures/texture.vs", "/home/test/share/C/opengl/resources/textures/texture.fs"); 
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -102,7 +102,7 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    unsigned char *data = stbi_load("/home/test/share/C/opengl/textures/resources/container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("/home/test/share/C/opengl/resources/textures/container.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
